4fc095ddeeac14b25cccce3fa4cf6bf7fdf3396a
n-i-x <nix@innocent.com>
2011-05-27 11:03:46 -0400

    Add CNAME and RM Manifest

---
+++ rommanager.js
@@ -0,0 +1,34 @@
+{
+  version: 1,
+  
+  
+  roms:
+  [
+    
+    {
+      name: "MIUI 1.5.27.0",
+      summary: "MIUI 1.5.27 in Chinese",
+      device: "passion",
+      
+      screenshots:
+        [
+          
+        ],
+      
+      product: "MIUI.us",
+      
+      incremental: 105270,
+      
+      modversion: "MIUI-1.5.27.0-N1-CN",
+      
+      urls:
+        [
+          
+          "http://www.roms.miui.us/n1/MIUIUS_N1_DEODEX_CHINESE.zip"
+          
+        ]
+    },
+    
+    
+  ]
+}
