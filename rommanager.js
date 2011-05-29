3802bd2bc859d3c276bb449b8f4c14f31bc3f49a
Daniel Charlton <charlton.danielk@gmail.com>
2011-05-28 23:15:26 -0700

    add more roms

--- rommanager.js
+++ rommanager.js
@@ -6,8 +6,8 @@
 	[
 	{
 		"device": "passion",
-		"name": "MIUI 1.5.27 (Chinese)",
-		"summary": "MIUI 1.5.27 in Chinese",
+		"name": "MIUI 1.5.27.0 (Chinese)",
+		"summary": "MIUI 1.5.27.0 in Chinese",
 		"product": "MIUI.us",
 		"incremental": "15270",
 		"modversion": "MIUI-1.5.27.0-N1-CN",
@@ -24,24 +24,17 @@
 		"urls": 
 		[
 		"http://roms.miui.us/1.5.27/MIUI.us_passion_1.5.27.0_CN_Deo_Signed.zip"
-		],
-
-		"addons": 
-		[
-		{
-			"name": "Google Apps",
-			"url": "http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip"        
-		}      
 		]
+
 	},
 
 	{
 		"device": "passion",
-		"name": "MIUI 1.5.27 (English)",
-		"summary": "MIUI 1.5.27 in English",
+		"name": "MIUI 1.5.27.1 (English)",
+		"summary": "MIUI 1.5.27.1 in English",
 		"product": "MIUI.us",
-		"incremental": "105270",
-		"modversion": "MIUI-1.5.27.0-N1-EN",
+		"incremental": "15271",
+		"modversion": "MIUI-1.5.27.1-N1-EN",
 		"screenshots":
 		[
 		"http://roms.miui.us/images/ss1.png",
@@ -53,24 +46,16 @@
 
 		"urls":
 		[
-		"http://roms.miui.us/1.5.27/MIUI.us_passion_1.5.27.0_Eng_Deo_Signed.zip"
-		],
-
-		"addons":
-		[
-		{
-			"name": "Google Apps",
-			"url": "http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip"        
-		}      
+		"http://roms.miui.us/1.5.27/MIUI.us_passion_1.5.27.1_Eng_Deo_ZipA_Signed.zip"
 		]
 	},
 
 	{
 		"device": "vision",
-		"name": "MIUI 1.5.27 (Chinese)",
-		"summary": "MIUI 1.5.27 in Chinese",
+		"name": "MIUI 1.5.27.0 (Chinese)",
+		"summary": "MIUI 1.5.27.0 in Chinese",
 		"product": "MIUI.us",
-		"incremental": "105270",
+		"incremental": "15270",
 		"modversion": "MIUI-1.5.27.0-VISION-CN",
 		"screenshots":
 		[
@@ -83,15 +68,29 @@
 
 		"urls":
 		[
-		"http://roms.miui.us/1.5.27/MIUI.us_Vision_1.5.27_CN_Deo_ZipA_Signed.zip",
+		"http://roms.miui.us/1.5.27/MIUI.us_vision_1.5.27.0_CN_Deo_Signed.zip",
+		]
+	},
+
+	{
+		"device": "vision",
+		"name": "MIUI 1.5.27.1 (English)",
+		"summary": "MIUI 1.5.27.1 in English",
+		"product": "MIUI.us",
+		"incremental": "15271",
+		"modversion": "MIUI-1.5.27.1-VISION-EN",
+		"screenshots":
+		[
+		"http://roms.miui.us/images/ss1.png",
+		"http://roms.miui.us/images/ss2.png",
+		"http://roms.miui.us/images/ss3.png",
+		"http://roms.miui.us/images/ss4.png",
+		"http://roms.miui.us/images/ss5.png"
 		],
 
-		"addons":
+		"urls":
 		[
-		{
-			"name": "Google Apps",
-			"url": "http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip" 
-		} 
+		"http://roms.miui.us/1.5.27/MIUI.us_vision_1.5.27.1_Eng_Deo_ZipA_Signed.zip",
 		]
 	},
 
@@ -114,15 +113,403 @@
 		"urls":
 		[
 		"http://roms.miui.us/1.5.27/MIUI.us_supersonic_1.5.27.1_Eng_Deo_ZipA_Signed_stiffspliff.zip",
+		]
+	},
+
+	{
+		"device": "supersonic",
+		"name": "MIUI 1.5.27.0 (Chinese)",
+		"summary": "MIUI 1.5.27.0 in Chinese",
+		"product": "MIUI.us",
+		"incremental": "15270",
+		"modversion": "MIUI-1.5.27.0-SUPERSONIC-CN",
+		"screenshots":
+		[
+		"http://roms.miui.us/images/ss1.png",
+		"http://roms.miui.us/images/ss2.png",
+		"http://roms.miui.us/images/ss3.png",
+		"http://roms.miui.us/images/ss4.png",
+		"http://roms.miui.us/images/ss5.png"
 		],
 
-		"addons":
+		"urls":
 		[
-		{
-			"name": "Google Apps",
-			"url": "http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip" 
-		} 
+		"http://roms.miui.us/1.5.27/MIUI.us_supersonic_1.5.27.1_CN_Deo_ZipA_Signed_stiffspliff.zip",
 		]
-	}
+	},
+
+	{
+		"device": "leo",
+		"name": "MIUI 1.5.27.1 (English)",
+		"summary": "MIUI 1.5.27.1 in English",
+		"product": "MIUI.us",
+		"incremental": "105271",
+		"modversion": "MIUI-1.5.27.1-LEO-EN",
+		"screenshots":
+		[
+		"http://roms.miui.us/images/ss1.png",
+		"http://roms.miui.us/images/ss2.png",
+		"http://roms.miui.us/images/ss3.png",
+		"http://roms.miui.us/images/ss4.png",
+		"http://roms.miui.us/images/ss5.png"
+		],
+
+		"urls":
+		[
+		"http://roms.miui.us/1.5.27/MIUI.us_ace_1.5.27.1_Eng_Deo_ZipA_Signed.zip",
+		]
+	},
+
+	{
+		"device": "leo",
+		"name": "MIUI 1.5.27.0 (Chinese)",
+		"summary": "MIUI 1.5.27.0 in Chinese",
+		"product": "MIUI.us",
+		"incremental": "15270",
+		"modversion": "MIUI-1.5.27.0-LEO-CN",
+		"screenshots":
+		[
+		"http://roms.miui.us/images/ss1.png",
+		"http://roms.miui.us/images/ss2.png",
+		"http://roms.miui.us/images/ss3.png",
+		"http://roms.miui.us/images/ss4.png",
+		"http://roms.miui.us/images/ss5.png"
+		],
+
+		"urls":
+		[
+		"http://roms.miui.us/1.5.27/MIUI.us_ace_1.5.27.0_CN_Deo_Signed.zip",
+		]
+	},
+
+	{
+		"device": "bravo",
+		"name": "MIUI 1.5.27.1 (English)",
+		"summary": "MIUI 1.5.27.1 in English",
+		"product": "MIUI.us",
+		"incremental": "105271",
+		"modversion": "MIUI-1.5.27.1-BRAVO-EN",
+		"screenshots":
+		[
+		"http://roms.miui.us/images/ss1.png",
+		"http://roms.miui.us/images/ss2.png",
+		"http://roms.miui.us/images/ss3.png",
+		"http://roms.miui.us/images/ss4.png",
+		"http://roms.miui.us/images/ss5.png"
+		],
+
+		"urls":
+		[
+		"http://roms.miui.us/1.5.27/MIUI.us_bravo_1.5.27.1_Eng_Deo_ZipA_Signed.zip",
+		]
+	},
+
+	{
+		"device": "bravo",
+		"name": "MIUI 1.5.27.0 (Chinese)",
+		"summary": "MIUI 1.5.27.0 in Chinese",
+		"product": "MIUI.us",
+		"incremental": "15270",
+		"modversion": "MIUI-1.5.27.0-BRAVO-CN",
+		"screenshots":
+		[
+		"http://roms.miui.us/images/ss1.png",
+		"http://roms.miui.us/images/ss2.png",
+		"http://roms.miui.us/images/ss3.png",
+		"http://roms.miui.us/images/ss4.png",
+		"http://roms.miui.us/images/ss5.png"
+		],
+
+		"urls":
+		[
+		"http://roms.miui.us/1.5.27/MIUI.us_bravo_1.5.27.0_CN_Deo_Signed.zip",
+		]
+	},
+
+	{
+		"device": "crespo",
+		"name": "MIUI 1.5.27.1 (English)",
+		"summary": "MIUI 1.5.27.1 in English",
+		"product": "MIUI.us",
+		"incremental": "105271",
+		"modversion": "MIUI-1.5.27.1-CRESPO-EN",
+		"screenshots":
+		[
+		"http://roms.miui.us/images/ss1.png",
+		"http://roms.miui.us/images/ss2.png",
+		"http://roms.miui.us/images/ss3.png",
+		"http://roms.miui.us/images/ss4.png",
+		"http://roms.miui.us/images/ss5.png"
+		],
+
+		"urls":
+		[
+		"http://roms.miui.us/1.5.27/MIUI.us_crespo_1.5.27.1_Eng_Deo_ZipA_Signed.zip",
+		]
+	},
+
+	{
+		"device": "crespo",
+		"name": "MIUI 1.5.27.0 (Chinese)",
+		"summary": "MIUI 1.5.27.0 in Chinese",
+		"product": "MIUI.us",
+		"incremental": "15270",
+		"modversion": "MIUI-1.5.27.0-CRESPO-CN",
+		"screenshots":
+		[
+		"http://roms.miui.us/images/ss1.png",
+		"http://roms.miui.us/images/ss2.png",
+		"http://roms.miui.us/images/ss3.png",
+		"http://roms.miui.us/images/ss4.png",
+		"http://roms.miui.us/images/ss5.png"
+		],
+
+		"urls":
+		[
+		"http://roms.miui.us/1.5.27/MIUI.us_crespo_1.5.27.0_CN_Deo_Signed.zip",
+		]
+	},
+
+	{
+		"device": "jordan",
+		"name": "MIUI 1.5.27.1 (English)",
+		"summary": "MIUI 1.5.27.1 in English",
+		"product": "MIUI.us",
+		"incremental": "105271",
+		"modversion": "MIUI-1.5.27.1-JORDAN-EN",
+		"screenshots":
+		[
+		"http://roms.miui.us/images/ss1.png",
+		"http://roms.miui.us/images/ss2.png",
+		"http://roms.miui.us/images/ss3.png",
+		"http://roms.miui.us/images/ss4.png",
+		"http://roms.miui.us/images/ss5.png"
+		],
+
+		"urls":
+		[
+		"http://roms.miui.us/1.5.27/MIUI.us_defy_1.5.27.1_Eng_Deo_ZipA_Signed.zip",
+		]
+	},
+
+	{
+		"device": "jordan",
+		"name": "MIUI 1.5.27.0 (Chinese)",
+		"summary": "MIUI 1.5.27.0 in Chinese",
+		"product": "MIUI.us",
+		"incremental": "15270",
+		"modversion": "MIUI-1.5.27.0-JORDAN-CN",
+		"screenshots":
+		[
+		"http://roms.miui.us/images/ss1.png",
+		"http://roms.miui.us/images/ss2.png",
+		"http://roms.miui.us/images/ss3.png",
+		"http://roms.miui.us/images/ss4.png",
+		"http://roms.miui.us/images/ss5.png"
+		],
+
+		"urls":
+		[
+		"http://roms.miui.us/1.5.27/MIUI.us_defy_1.5.27.0_CN_Deo_Signed.zip",
+		]
+	},
+
+	{
+		"device": "i9000",
+		"name": "MIUI 1.5.27.1 (English)",
+		"summary": "MIUI 1.5.27.1 in English",
+		"product": "MIUI.us",
+		"incremental": "105271",
+		"modversion": "MIUI-1.5.27.1-I9000-EN",
+		"screenshots":
+		[
+		"http://roms.miui.us/images/ss1.png",
+		"http://roms.miui.us/images/ss2.png",
+		"http://roms.miui.us/images/ss3.png",
+		"http://roms.miui.us/images/ss4.png",
+		"http://roms.miui.us/images/ss5.png"
+		],
+
+		"urls":
+		[
+		"http://roms.miui.us/1.5.27/MIUI.us_I9000_1.5.27.1_Eng_Deo_ZipA_Signed.zip",
+		]
+	},
+
+	{
+		"device": "i9000",
+		"name": "MIUI 1.5.27.0 (Chinese)",
+		"summary": "MIUI 1.5.27.0 in Chinese",
+		"product": "MIUI.us",
+		"incremental": "15270",
+		"modversion": "MIUI-1.5.27.0-I9000-CN",
+		"screenshots":
+		[
+		"http://roms.miui.us/images/ss1.png",
+		"http://roms.miui.us/images/ss2.png",
+		"http://roms.miui.us/images/ss3.png",
+		"http://roms.miui.us/images/ss4.png",
+		"http://roms.miui.us/images/ss5.png"
+		],
+
+		"urls":
+		[
+		"http://roms.miui.us/1.5.27/MIUI.us_I9000_1.5.27.0_CN_Deo_Signed.zip",
+		]
+	},
+
+	{
+		"device": "incrediblec",
+		"name": "MIUI 1.5.27.1 (English)",
+		"summary": "MIUI 1.5.27.1 in English",
+		"product": "MIUI.us",
+		"incremental": "105271",
+		"modversion": "MIUI-1.5.27.1-INCREDIBLEC-EN",
+		"screenshots":
+		[
+		"http://roms.miui.us/images/ss1.png",
+		"http://roms.miui.us/images/ss2.png",
+		"http://roms.miui.us/images/ss3.png",
+		"http://roms.miui.us/images/ss4.png",
+		"http://roms.miui.us/images/ss5.png"
+		],
+
+		"urls":
+		[
+		"http://roms.miui.us/1.5.27/MIUI.us_incrediblec_1.5.27.1_Eng_Deo_ZipA_Signed_leeech.zip",
+		]
+	},
+
+	{
+		"device": "solana",
+		"name": "MIUI 1.5.27.1 (English)",
+		"summary": "MIUI 1.5.27.1 in English",
+		"product": "MIUI.us",
+		"incremental": "105271",
+		"modversion": "MIUI-1.5.27.1-SOLANA-EN",
+		"screenshots":
+		[
+		"http://roms.miui.us/images/ss1.png",
+		"http://roms.miui.us/images/ss2.png",
+		"http://roms.miui.us/images/ss3.png",
+		"http://roms.miui.us/images/ss4.png",
+		"http://roms.miui.us/images/ss5.png"
+		],
+
+		"urls":
+		[
+		"http://roms.miui.us/1.5.27/MIUI.us_milestone_1.5.27.1_Eng_Deo_ZipA_Signed.zip",
+		]
+	},
+
+	{
+		"device": "solana",
+		"name": "MIUI 1.5.27.0 (Chinese)",
+		"summary": "MIUI 1.5.27.0 in Chinese",
+		"product": "MIUI.us",
+		"incremental": "15270",
+		"modversion": "MIUI-1.5.27.0-SOLANA-CN",
+		"screenshots":
+		[
+		"http://roms.miui.us/images/ss1.png",
+		"http://roms.miui.us/images/ss2.png",
+		"http://roms.miui.us/images/ss3.png",
+		"http://roms.miui.us/images/ss4.png",
+		"http://roms.miui.us/images/ss5.png"
+		],
+
+		"urls":
+		[
+		"http://roms.miui.us/1.5.27/MIUI.us_milestone_1.5.27.0_CN_Deo_Signed.zip",
+		]
+	},
+
+	{
+		"device": "ace",
+		"name": "MIUI 1.5.27.1 (English)",
+		"summary": "MIUI 1.5.27.1 in English",
+		"product": "MIUI.us",
+		"incremental": "105271",
+		"modversion": "MIUI-1.5.27.1-ACE-EN",
+		"screenshots":
+		[
+		"http://roms.miui.us/images/ss1.png",
+		"http://roms.miui.us/images/ss2.png",
+		"http://roms.miui.us/images/ss3.png",
+		"http://roms.miui.us/images/ss4.png",
+		"http://roms.miui.us/images/ss5.png"
+		],
+
+		"urls":
+		[
+		"http://roms.miui.us/1.5.27/MIUI.us_spade_1.5.27.1_Eng_Deo_ZipA_Signed.zip",
+		]
+	},
+
+	{
+		"device": "ace",
+		"name": "MIUI 1.5.27.0 (Chinese)",
+		"summary": "MIUI 1.5.27.0 in Chinese",
+		"product": "MIUI.us",
+		"incremental": "15270",
+		"modversion": "MIUI-1.5.27.0-ACE-CN",
+		"screenshots":
+		[
+		"http://roms.miui.us/images/ss1.png",
+		"http://roms.miui.us/images/ss2.png",
+		"http://roms.miui.us/images/ss3.png",
+		"http://roms.miui.us/images/ss4.png",
+		"http://roms.miui.us/images/ss5.png"
+		],
+
+		"urls":
+		[
+		"http://roms.miui.us/1.5.27/MIUI.us_spade_1.5.27.0_CN_Deo_Signed.zip",
+		]
+	},
+
+	{
+		"device": "t959",
+		"name": "MIUI 1.5.27.1 (English)",
+		"summary": "MIUI 1.5.27.1 in English",
+		"product": "MIUI.us",
+		"incremental": "105271",
+		"modversion": "MIUI-1.5.27.1-T959-EN",
+		"screenshots":
+		[
+		"http://roms.miui.us/images/ss1.png",
+		"http://roms.miui.us/images/ss2.png",
+		"http://roms.miui.us/images/ss3.png",
+		"http://roms.miui.us/images/ss4.png",
+		"http://roms.miui.us/images/ss5.png"
+		],
+
+		"urls":
+		[
+		"http://roms.miui.us/1.5.27/MIUI.us_T959_1.5.27.1_Eng_Deo_ZipA_Signed.zip",
+		]
+	},
+
+	{
+		"device": "t959",
+		"name": "MIUI 1.5.27.0 (Chinese)",
+		"summary": "MIUI 1.5.27.0 in Chinese",
+		"product": "MIUI.us",
+		"incremental": "15270",
+		"modversion": "MIUI-1.5.27.0-T959-CN",
+		"screenshots":
+		[
+		"http://roms.miui.us/images/ss1.png",
+		"http://roms.miui.us/images/ss2.png",
+		"http://roms.miui.us/images/ss3.png",
+		"http://roms.miui.us/images/ss4.png",
+		"http://roms.miui.us/images/ss5.png"
+		],
+
+		"urls":
+		[
+		"http://roms.miui.us/1.5.27/MIUI.us_T959_1.5.27.0_CN_Deo_Signed.zip",
+		]
+	},
 	]
 }
