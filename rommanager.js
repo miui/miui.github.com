9e407f375c64dfa3c7850a5f45d5b6bc688dd0ad
Daniel Charlton <charlton.danielk@gmail.com>
2011-05-28 22:19:45 -0700

    Add more to manifest

--- rommanager.js
+++ rommanager.js
@@ -1,34 +1,73 @@
 {
-  version: 1,
-  
-  
-  roms:
-  [
-    
-    {
-      name: "MIUI 1.5.27.0",
-      summary: "MIUI 1.5.27 in Chinese",
-      device: "passion",
-      
-      screenshots:
-        [
-          
+  "version": "1",
+  "homepage": "http://miui.us/",
+  "roms":
+    [
+      {
+        "device": "passion",
+        "name": "MIUI 1.5.27 (Chinese)",
+        "summary": "MIUI 1.5.27 in Chinese",
+        "product": "MIUI.us",
+        "incremental": "105270",
+        "modversion": "MIUI-1.5.27.0-N1-CN",
+        "screenshots": [
+          "http://roms.miui.us/images/ss1.png",
+          "http://roms.miui.us/images/ss2.png",
+          "http://roms.miui.us/images/ss3.png",
+          "http://roms.miui.us/images/ss4.png",
+          "http://roms.miui.us/images/ss5.png"
         ],
-      
-      product: "MIUI.us",
-      
-      incremental: 105270,
-      
-      modversion: "MIUI-1.5.27.0-N1-CN",
-      
-      urls:
-        [
-          
-          "http://www.roms.miui.us/n1/MIUIUS_N1_DEODEX_CHINESE.zip"
-          
+        "url": "http://roms.miui.us/1.5.27/MIUI.us_passion_1.5.27.0_CN_Deo_Signed.zip"
+        "addons": [
+          {
+            "name": "Google Apps",
+            "url": "http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip"        
+          }      
         ]
-    },
-    
-    
+      },
+      {
+        "device": "passion",
+        "name": "MIUI 1.5.27 (English)",
+        "summary": "MIUI 1.5.27 in English",
+        "product": "MIUI.us",
+        "incremental": "105270",
+        "modversion": "MIUI-1.5.27.0-N1-EN",
+        "screenshots": [
+          "http://roms.miui.us/images/ss1.png",
+          "http://roms.miui.us/images/ss2.png",
+          "http://roms.miui.us/images/ss3.png",
+          "http://roms.miui.us/images/ss4.png",
+          "http://roms.miui.us/images/ss5.png"
+        ],
+        "url": "http://roms.miui.us/1.5.27/MIUI.us_passion_1.5.27.0_Eng_Deo_Signed.zip"
+        "addons": [
+          {
+            "name": "Google Apps",
+            "url": "http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip"        
+          }      
+        ]
+      },
+      {
+        "device": "vision",
+        "name": "MIUI 1.5.27 (Chinese)",
+        "summary": "MIUI 1.5.27 in Chinese",
+        "product": "MIUI.us",
+        "incremental": "105270",
+        "modversion": "MIUI-1.5.27.0-VISION-CN",
+        "screenshots": [
+          "http://roms.miui.us/images/ss1.png",
+          "http://roms.miui.us/images/ss2.png",
+          "http://roms.miui.us/images/ss3.png",
+          "http://roms.miui.us/images/ss4.png",
+          "http://roms.miui.us/images/ss5.png"
+        ],
+        "url": "http://roms.miui.us/1.5.27/MIUI.us_Vision_1.5.27_CN_Deo_ZipA_Signed.zip",
+        "addons": [
+          {
+            "name": "Google Apps",
+            "url": "http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip" 
+          } 
+        ]
+      }
   ]
 }
