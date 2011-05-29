34a0289c5f218c7f2f31773cbf3354b901dbab1e
Daniel Charlton <charlton.danielk@gmail.com>
2011-05-28 22:29:19 -0700

    fix?

--- rommanager.js
+++ rommanager.js
@@ -1,93 +1,93 @@
 {
-  "version": "1",
-  "homepage": "http://miui.us/",
-  "roms":
+  version: 1,
+  homepage: "http://miui.us/",
+  roms:
     [
       {
-        "device": "passion",
-        "name": "MIUI 1.5.27 (Chinese)",
-        "summary": "MIUI 1.5.27 in Chinese",
-        "product": "MIUI.us",
-        "incremental": "105270",
-        "modversion": "MIUI-1.5.27.0-N1-CN",
-        "screenshots": [
+        device: "passion",
+        name: "MIUI 1.5.27 (Chinese)",
+        summary: "MIUI 1.5.27 in Chinese",
+        product: "MIUI.us",
+        incremental: "105270",
+        modversion: "MIUI-1.5.27.0-N1-CN",
+        screenshots: [
           "http://roms.miui.us/images/ss1.png",
           "http://roms.miui.us/images/ss2.png",
           "http://roms.miui.us/images/ss3.png",
           "http://roms.miui.us/images/ss4.png",
           "http://roms.miui.us/images/ss5.png"
         ],
-        "url": "http://roms.miui.us/1.5.27/MIUI.us_passion_1.5.27.0_CN_Deo_Signed.zip"
-        "addons": [
+        url: "http://roms.miui.us/1.5.27/MIUI.us_passion_1.5.27.0_CN_Deo_Signed.zip"
+        addons: [
           {
-            "name": "Google Apps",
-            "url": "http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip"        
+            name: "Google Apps",
+            url: "http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip"        
           }      
         ]
       },
       {
-        "device": "passion",
-        "name": "MIUI 1.5.27 (English)",
-        "summary": "MIUI 1.5.27 in English",
-        "product": "MIUI.us",
-        "incremental": "105270",
-        "modversion": "MIUI-1.5.27.0-N1-EN",
-        "screenshots": [
+        device: "passion",
+        name: "MIUI 1.5.27 (English)",
+        summary: "MIUI 1.5.27 in English",
+        product: "MIUI.us",
+        incremental: "105270",
+        modversion: "MIUI-1.5.27.0-N1-EN",
+        screenshots: [
           "http://roms.miui.us/images/ss1.png",
           "http://roms.miui.us/images/ss2.png",
           "http://roms.miui.us/images/ss3.png",
           "http://roms.miui.us/images/ss4.png",
           "http://roms.miui.us/images/ss5.png"
         ],
-        "url": "http://roms.miui.us/1.5.27/MIUI.us_passion_1.5.27.0_Eng_Deo_Signed.zip"
-        "addons": [
+        url: "http://roms.miui.us/1.5.27/MIUI.us_passion_1.5.27.0_Eng_Deo_Signed.zip"
+        addons: [
           {
-            "name": "Google Apps",
-            "url": "http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip"        
+            name: "Google Apps",
+            url: "http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip"        
           }      
         ]
       },
       {
-        "device": "vision",
-        "name": "MIUI 1.5.27 (Chinese)",
-        "summary": "MIUI 1.5.27 in Chinese",
-        "product": "MIUI.us",
-        "incremental": "105270",
-        "modversion": "MIUI-1.5.27.0-VISION-CN",
-        "screenshots": [
+        device: "vision",
+        name: "MIUI 1.5.27 (Chinese)",
+        summary: "MIUI 1.5.27 in Chinese",
+        product: "MIUI.us",
+        incremental: "105270",
+        modversion: "MIUI-1.5.27.0-VISION-CN",
+        screenshots: [
           "http://roms.miui.us/images/ss1.png",
           "http://roms.miui.us/images/ss2.png",
           "http://roms.miui.us/images/ss3.png",
           "http://roms.miui.us/images/ss4.png",
           "http://roms.miui.us/images/ss5.png"
         ],
-        "url": "http://roms.miui.us/1.5.27/MIUI.us_Vision_1.5.27_CN_Deo_ZipA_Signed.zip",
-        "addons": [
+        url: "http://roms.miui.us/1.5.27/MIUI.us_Vision_1.5.27_CN_Deo_ZipA_Signed.zip",
+        addons: [
           {
-            "name": "Google Apps",
-            "url": "http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip" 
+            name: "Google Apps",
+            url: "http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip" 
           } 
         ]
       }
       {
-        "device": "supersonic",
-        "name": "MIUI 1.5.27.1 (English)",
-        "summary": "MIUI 1.5.27.1 in English",
-        "product": "MIUI.us",
-        "incremental": "105271",
-        "modversion": "MIUI-1.5.27.1-SUPERSONIC-EN",
-        "screenshots": [
+        device: "supersonic",
+        name: "MIUI 1.5.27.1 (English)",
+        summary: "MIUI 1.5.27.1 in English",
+        product: "MIUI.us",
+        incremental: "105271",
+        modversion: "MIUI-1.5.27.1-SUPERSONIC-EN",
+        screenshots: [
           "http://roms.miui.us/images/ss1.png",
           "http://roms.miui.us/images/ss2.png",
           "http://roms.miui.us/images/ss3.png",
           "http://roms.miui.us/images/ss4.png",
           "http://roms.miui.us/images/ss5.png"
         ],
-        "url": "http://roms.miui.us/1.5.27/MIUI.us_supersonic_1.5.27.1_Eng_Deo_ZipA_Signed_stiffspliff.zip",
-        "addons": [
+        url: "http://roms.miui.us/1.5.27/MIUI.us_supersonic_1.5.27.1_Eng_Deo_ZipA_Signed_stiffspliff.zip",
+        addons: [
           {
-            "name": "Google Apps",
-            "url": "http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip" 
+            name: "Google Apps",
+            url: "http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip" 
           } 
         ]
       }
